8d4a0e1ccc83325fd5cb19de9d7575e439814eb7
sreysus <sreyassabbani@gmail.com>
2025-02-06 10:25:15 -0500

    wip refactor: store & display state better
    - `enum BoardState` modified, new `struct Game` to hold more information about state
    - correspondingly update `check_board_state` (now named `update_game_state`)
    - some bugs: can click on already drawn square, can click after game has been won

--- main.c
+++ main.c
@@ -25,17 +25,26 @@
 
 // Players, "x" and "o"
 typedef enum Player {
-  Circle = 1,
-  Cross = 2
+  Blue = 1,
+  Red = 2
 } Player;
 
 
 // Record board state
 // note that `Playing` is falsy
-typedef enum BoardState {
+typedef enum {
   Playing = 0,
-  Won = 1
+  BlueWon = 1,
+  RedWon = 2,
+  Draw = 3
 } BoardState;
+
+typedef struct {
+  BoardState state;
+  uint8_t* board;
+  Player turn;
+  uint8_t moves_left;
+} Game;
 
 
 void rerender_background(SDL_Renderer* renderer, uint8_t r, uint8_t g, uint8_t b) {
@@ -63,70 +72,72 @@ void render_block(SDL_Renderer* renderer, SDL_Point* position, uint8_t r, uint8_
 }
 
 
-void draw_circle(SDL_Renderer* renderer, SDL_Point* position) {
+void draw_blue(SDL_Renderer* renderer, SDL_Point* position) {
   render_block(renderer, position, BLUE);
 }
 
 
-void draw_cross(SDL_Renderer* renderer, SDL_Point* position) {
+void draw_red(SDL_Renderer* renderer, SDL_Point* position) {
   render_block(renderer, position, RED);
 }
 
 
-int8_t check_board_state(int8_t* board, int8_t new_position) {
+void update_game_state(Game* game, int8_t new_position) {
   // Note: checking only the rows/columns/diagonals that have been updated allows us to omit checking for triplets of 0
   
-  // Calculations turn out this way because `board` follows row-major order
+  // Calculations turn out this way because `game->board` follows row-major order
   int y = new_position / 3;
   int x = new_position % 3;
   
   // Check vertically
-  int8_t vertical_match = board[x];
+  int8_t vertical_match = game->board[x];
   for (int i = 1; i < 3; ++i) {
-    if (vertical_match != board[x + 3 * i]) vertical_match = -1;
+    if (vertical_match != game->board[x + 3 * i]) vertical_match = 0;
   }
   
   // Return if a vertical triplet is found
-  if (vertical_match != -1) return (Player) vertical_match;
+  if (vertical_match != 0) game->state = (BoardState) vertical_match;
 
   // Check horizontally
-  int8_t horizontal_match = board[3 * y]; 
+  int8_t horizontal_match = game->board[3 * y]; 
   for (int i = 1; i < 3; ++i) {
-    if (horizontal_match != board[3 * y + i]) horizontal_match = -1;
+    if (horizontal_match != game->board[3 * y + i]) horizontal_match = 0;
   }
 
   // Return if a horizontal triplet is found
-  if (horizontal_match != -1) return (Player) horizontal_match;
+  if (horizontal_match != 0) game->state = (BoardState) horizontal_match;
 
   // Check diagonally (0-indexed row-major `new_position` modulo 2 gives the pattern we are looking for; draw it)
   // Also equivalent to binary operation `new_position & 1`
   if (new_position % 2 == 0) {
-    int8_t diagonal_match = board[2];
+    int8_t diagonal_match = game->board[2];
     
     // Top-right to bottom-left
     if (new_position % 2 == 0 && new_position % 8 != 0) {
       for (int i = 4; i <= 3 * 2; i += 2) {
-        if (diagonal_match != board[i]) diagonal_match = -1;
+        if (diagonal_match != game->board[i]) diagonal_match = 0;
       }
-      if (diagonal_match != -1) return (Player) diagonal_match;
+      if (diagonal_match != 0) game->state = (BoardState) diagonal_match;
     }
     
     // Top-left to bottom-right
     if (new_position % 4 == 0) {
-      diagonal_match = board[0];
+      diagonal_match = game->board[0];
       for (int i = 4; i < 3 * 4; i += 4) {
-        if (diagonal_match != board[i]) diagonal_match = -1;    
+        if (diagonal_match != game->board[i]) diagonal_match = 0;    
       }
-      if (diagonal_match != -1) return (Player) diagonal_match;
+      if (diagonal_match != 0) game->state = (BoardState) diagonal_match;
     }
   }
 
-  // No triplets found
-  return -1;
+  // If moves left is 0 and no match is found, set state to `Draw`
+  if (game->moves_left == 0) game->state = Draw;
+
+  // No triplets found, BoardState remains `Playing`
 }
 
 
-void draw_gameboard(SDL_Renderer* renderer, int8_t* board) {
+void draw_gameboard(SDL_Renderer* renderer, uint8_t* board) {
   // row-major order
   SDL_Point squares[9];
   int i = 0;
@@ -141,10 +152,10 @@ void draw_gameboard(SDL_Renderer* renderer, int8_t* board) {
           render_block(renderer, &squares[i], GRAY);
           break;
         case 1:
-          draw_circle(renderer, &squares[i]);
+          draw_blue(renderer, &squares[i]);
           break;
         case 2:
-          draw_cross(renderer, &squares[i]);
+          draw_red(renderer, &squares[i]);
           break;
       }
       ++i;
@@ -163,10 +174,11 @@ int8_t position_on_board(int mouse_x, int mouse_y) {
 }
 
 
-void move(SDL_Renderer* renderer, int8_t* board, Player player, uint8_t position) {
+void move(SDL_Renderer* renderer, uint8_t* board, uint8_t* moves_left, Player player, uint8_t position) {
   if (board[position] == 0) {
     board[position] = player;
   }
+  --*moves_left;
 }
 
 void render_text(SDL_Renderer* renderer, TTF_Font* font, const char *text, SDL_Point point) {
@@ -234,30 +246,45 @@ int main() {
   int init = 1;
   int mouse_x, mouse_y;
 
-  // all values are zero
-  int8_t board[9] = { 0 };
-  BoardState state;
-  state = Playing;
+  Game game = {
+    .state = Playing,
+    // Idk who goes first
+    .turn = Red,
+    // all values are zero
+    .board = (uint8_t[9]){ 0 },
+    .moves_left = 9,
+  };
 
-  // Idk who goes first
-  Player turn = Cross;
 
-  while (running && !state) {
+  while (running) {
 
     // redraw background for next frame
     SDL_SetRenderDrawColor(renderer, WHITE, 255);
     SDL_RenderClear(renderer);
 
     // redraw gameboard
-    draw_gameboard(renderer, board);
+    draw_gameboard(renderer, game.board);
 
-    // display turn
-    switch (turn) {
-      case Circle:
-        render_caption(renderer, font, "Player Cross");
+    switch (game.state) {
+      case Playing:
+        // display turn
+        switch (game.turn) {
+          case Red:
+            render_caption(renderer, font, "Player Red's turn!");
+            break;
+          case Blue:
+            render_caption(renderer, font, "Player Blue's turn!");
+            break;
+        }
         break;
-      case Cross:
-        render_caption(renderer, font, "Player Circle");
+      case BlueWon:
+        render_caption(renderer, font, "Blue Won!");
+        break;
+      case RedWon:
+        render_caption(renderer, font, "Red won!");
+        break;
+      case Draw:
+        render_caption(renderer, font, "Game drawn!");
         break;
     }
 
@@ -272,23 +299,24 @@ int main() {
       if (event.type == SDL_MOUSEBUTTONDOWN) {
         SDL_GetGlobalMouseState(&mouse_x, &mouse_y);
         int8_t i = position_on_board(mouse_x, mouse_y);
+
         switch (event.button.button) {
           case SDL_BUTTON_LEFT:
-            if (turn == Cross) {
-              move(renderer, board, Cross, i);
-              if (check_board_state(board, i) != -1) state = Won;
-              turn = Circle;
-              render_caption(renderer, font, "Player Circle");
-              printf("Circle turn");
+            if (game.turn == Red) {
+              move(renderer, game.board, &game.moves_left, Red, i);
+              update_game_state(&game, i);
+              game.turn = Blue;
+              render_caption(renderer, font, "Player Blue");
+              printf("Blue turn\n");
               fflush(stdout);
             }
             break;
           case SDL_BUTTON_RIGHT:
-            if (turn == Circle) {
-              move(renderer, board, Circle, i);
-              if (check_board_state(board, i) != -1) state = Won;
-              turn = Cross;
-              printf("Cross turn");
+            if (game.turn == Blue) {
+              move(renderer, game.board, &game.moves_left, Blue, i);
+              update_game_state(&game, i);
+              game.turn = Red;
+              printf("Red turn\n");
               fflush(stdout);
             }
             break;
